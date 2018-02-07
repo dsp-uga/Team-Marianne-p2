611379060c1ab0d45932c66446645cf4d8892d53
ankit-vaghela30 <ankit.vaghela30@gmail.com>
2018-02-07 06:56:31 -0500

    Preprocessing for the training dta done

--- src/ByteFeature.py
+++ src/ByteFeature.py
@@ -5,13 +5,13 @@ from pyspark.mllib.tree import DecisionTree
 from sys import argv
 import numpy as np
 import re
-import urllib2
+#import urllib2
 import csv
 from nltk.util import ngrams
 import itertools
 from collections import Counter
 
-class preprocessor(object):
+class PreProcessor:
     '''
     preprocess byte data for microsoft malware detection project
       convert byte file and labels into a sparse matrix
@@ -20,34 +20,10 @@ class preprocessor(object):
         - y: pyspark rdd, with (id, label) format
     '''
 
-    def __init__(self, grams=[1, 2], freqThreshold=10):
+    def __init__(self, ctx,  grams=[1, 2], freqThreshold=10):
         self.grams = grams
         self.freqThreshold = freqThreshold
-
-    # helper methods
-    def stripFileNames(self, stringOfName):
-        splits = stringOfName.split("/")
-        name = splits[-1][:20]
-        return name
-
-    # token a document, only keep 2-digit code, and its grams
-    def tokenEachDoc(self, aDoc):
-        '''
-        return a dictionary of item-freq, here items are single words and grams
-        '''
-        tmpWordList = [x for x in re.sub('\\\\r\\\\n', ' ', aDoc).split() if len(x) == 2 and x != '??']
-        tmpGramList = []
-        for i in xrange(len(self.grams)):
-            tmpGramList.append([''.join(x) for x in list(ngrams(tmpWordList, self.grams[i]))])
-
-        # here tmpGramList is a list of list, here we should remove the inner lists
-        sumGramList = list(itertools.chain.from_iterable(tmpGramList))  # this is a very long list, depends on the gram numbers
-        sumGramDict = dict(Counter(sumGramList))
-
-        for keys in sumGramDict.keys():
-            if sumGramDict[keys] < self.freqThreshold:
-                del sumGramDict[keys]
-        return sumGramDict
+        self.ctx = ctx
 
     def byteFeatureGenerator(self, X, y):  # format, (id, dictionary of items)
         '''
@@ -58,21 +34,6 @@ class preprocessor(object):
             .join(y)
 
         return tokenizedX
-
-    def convertHexToInt(self, hexStr):
-        '''
-        convert all hex number to 1-65792, which is one by one.
-        '''
-        if len(hexStr) == 2:
-            return (int(str(hexStr), 16)+1)
-        else:
-            return (int('1' + str(hexStr), 16)-65279)
-
-    def convertDict(self, textDict):
-        tmp = {}
-        for oldKey, value in textDict.items():
-            tmp[self.convertHexToInt(oldKey)] = value
-        return tmp
 
     def convertToSVMFormat(self, Str):
         '''
@@ -93,24 +54,99 @@ class preprocessor(object):
         newStr = re.sub('[\(\)]', '', newStr)
         return newStr
 
+    def load_data(self, data_path, labels_path=None):
+        '''Load the training and testing data from given path
+        '''
+        data = self.ctx.textFile(data_path)
+        data = data.zipWithIndex().map(lambda x: (x[1], x[0])) # (id, hash)
+        if labels_path is not None:
+            labels = self.ctx.textFile(labels_path)
+            labels = labels.zipWithIndex().map(lambda x: (x[1], x[0])) # (id, label)
+        else:
+            labels = None
+        return data, labels
 
+    def extract_data(self, hash_list, byte_files_path):
+        '''Extracts byte files of hash number from the path provided and adds
+        all up in one single rdd
+        '''
+        byte_files = self.ctx.wholeTextFiles(str(byte_files_path)+str(hash_list[0])+'.bytes')
+        for hash in hash_list[1:]:
+            byte_files += self.ctx.wholeTextFiles(str(byte_files_path)+str(hash)+'.bytes')
+        return byte_files #(hash, byte file)
 
+    def transform_data(self, data, byte_files_path, labels=None):
+        '''Loads the actual data, Extracts features out of it and maps labels with file names i.e. hash
+        '''
+        if labels is not None:
+            labels = data.join(labels) # (id, (hash, label))
+            labels = labels.map(lambda x: (x[1][0], x[1][1])) # (hashm label)
+        else:
+            labels = None
+        #self.ngrams = self.ctx.broadcast(self.grams)
+        hash_list = data.values().collect()
+        byte_files = self.extract_data(hash_list, byte_files_path)
+
+        def stripFileNames(stringOfName):
+            splits = stringOfName.split("/")
+            name = splits[-1][:20]
+            return name
+
+        def tokenEachDoc(aDoc):
+            '''
+            return a dictionary of item-freq, here items are single words and grams
+            '''
+            tmpWordList = [x for x in re.sub('\\\\r\\\\n', ' ', aDoc).split() if len(x) == 2 and x != '??']
+            tmpGramList = []
+            grams = [1,2]
+            for i in range(len([1,2])):
+                tmpGramList.append([''.join(x) for x in list(ngrams(tmpWordList, grams[i]))])
+
+            # here tmpGramList is a list of list, here we should remove the inner lists
+            sumGramList = list(itertools.chain.from_iterable(tmpGramList))  # this is a very long list, depends on the gram numbers
+            sumGramDict = dict(Counter(sumGramList))
+            retDec = {}
+            for keys in sumGramDict.keys():
+                if sumGramDict[keys] < 10:
+                    #del sumGramDict[keys]
+                    retDec[keys] = sumGramDict[keys]
+            return retDec
+        data = byte_files.map(lambda x: (stripFileNames(x[0]), tokenEachDoc(x[1]))) # (hash, bigrams_dict)
+
+        def convertHexToInt(hexStr):
+            '''
+            convert all hex number to 1-65792, which is one by one.
+            '''
+            if len(hexStr) == 2:
+                return (int(str(hexStr), 16)+1)
+            else:
+                return (int('1' + str(hexStr), 16)-65279)
+
+        def convertDict(textDict):
+            tmp = {}
+            for oldKey, value in textDict.items():
+                tmp[convertHexToInt(oldKey)] = value
+            return tmp
+        data = data.map(lambda x: (x[0], convertDict(x[1]))) # (hash, bigrams_dict)-- bigram_dict's key is integer now
+        return data, labels
+
+'''
 sc= SparkContext.getOrCreate()
 prc = preprocessor()
 byteFiles = sc.wholeTextFiles("TrainBytes/")
 byteTestFiles = sc.wholeTextFiles("TestBytes/")
 
 trainFileIndex = sc.textFile("X_vs_train.txt").zipWithIndex()\
-        .map(lambda x: (x[1], x[0]))
+        .map(lambda x: (x[1], x[0])) #(id, hash_name)
 trainFileLabel = sc.textFile("y_vs_train.txt").zipWithIndex()\
-        .map(lambda x: (x[1], x[0]))
+        .map(lambda x: (x[1], x[0])) #(id, label)
 
 testFileIndex = sc.textFile("X_vs_test.txt").zipWithIndex()\
-        .map(lambda x: (x[1], x[0]))
+        .map(lambda x: (x[1], x[0])) #(id, hash_name)
 
 labelRDD = trainFileIndex.join(trainFileLabel)\
-        .map(lambda x: (x[1][0], x[1][1]))
-labelRDDTest = testFileIndex.map(lambda x: (x[1], '1'))
+        .map(lambda x: (x[1][0], x[1][1])) # (hash_name, label)
+labelRDDTest = testFileIndex.map(lambda x: (x[1], '1')) # (hash_name, 1)
 
 trainData = byteFiles.map(lambda x: (prc.stripFileNames(x[0]), prc.tokenEachDoc(x[1])))\
         .map(lambda x: (x[0], prc.convertDict(x[1])))\
@@ -125,3 +161,4 @@ testData = byteTestFiles.map(lambda x: (prc.stripFileNames(x[0]), prc.tokenEachD
         .map(lambda x: (str(x[1][1]) + '-' + str(x[0]), sorted(x[1][0].iteritems(), key = lambda d:d[0])))\
         .map(lambda x: (x[0], prc.convertToSVMFormat(str(x[1]))))\
         .map(lambda x: prc.convertToSVMFormat2(str(x)))
+'''
