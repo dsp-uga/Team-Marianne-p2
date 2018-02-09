8b0a20880c44918da7e9aca4aaa11fbb5da0d5c5
Vinay Kumar Bingi <vinay.bingi@gmail.com>
2018-02-09 04:34:26 -0500

    adds a driver file that calls all the utility and classifier functions

--- src/__main__.py
+++ src/__main__.py
@@ -1,46 +1,18 @@
+
 from pyspark import SparkContext, SparkConf
-import argparse
-import src.byte_feature as src
 
-def preprocess(args):
-    '''Test and verify the preprocessing
-    '''
-    sc= SparkContext.getOrCreate()
-    train_x = args.train_x
-    train_y = args.train_y
-    test_x = args.test_x
-    test_y = args.test_y
-    byte_files_path = args.byte_files_path
+from src.preprocess.BytePreProcessing import ByteFeatures
 
-    train_x, train_y = src.PreProcessor(sc).load_data(train_x, train_y)
-    transform_x, transform_y = src.PreProcessor(sc).transform_data(train_x, byte_files_path, train_y)
-    src.PreProcessor(sc).write_to_file(transform_x, transform_y)
+def main(args):
+	
+	# Refer https://spark.apache.org/docs/latest/configuration.html for additional changes to config 
+	config = SparkConf().setAppName("team-marianne-p2")\
+				.set("spark.hadoop.validateOutputSpecs", "false")
 
-    train_x, train_y = src.PreProcessor(sc).load_data(test_x, train_y)
-    transform_x, transform_y = src.PreProcessor(sc).transform_data(train_x, byte_files_path, train_y)
-    src.PreProcessor(sc).write_to_file(transform_x, transform_y)
+	sc = SparkContext.getOrCreate(config)
 
-    print('transformed data is: ', transform_x.collectAsMap())
-    print('transformed label is: ', transform_y.collectAsMap())
-
-def main():
-    parser = argparse.ArgumentParser(description='Execute Commands')
-    subcommands = parser.add_subparsers()
-
-    # src preprocess <train_x> <train_y> <test_x> [<test_y>]
-    cmd = subcommands.add_parser('preprocess', description= 'Preprocess')
-    cmd.add_argument('train_x', help='path of the training data')
-    cmd.add_argument('train_y', help='path of the training labels')
-    cmd.add_argument('test_x', help='path of thetesting data')
-    cmd.add_argument('test_y', help='path of the testing labels', nargs='?', default=None)
-    cmd.add_argument('byte_files_path', help='path of the byte files')
-    cmd.set_defaults(func=preprocess)
-
-    args = parser.parse_args()
-    if hasattr(args, 'func'):
-        args.func(args)
-    else:
-        parser.print_help()
-
-if __name__ == '__main__':
-    main()
+	data, labels = ByteFeatures(sc).load_data(args.dataset, args.labels)	# converts byte data and labels to RDDs
+	# updates data and labels rdds. labels will be updated to (hash, label) and data will be updated to (hash, bigram_dict)
+	data, labels = ByteFeatures(sc).transform_data(data, args.bytestrain, labels)
+	# write data rdd to file -> makes it easier to retrieve processed data
+	ByteFeatures(sc).write_to_file(data, args.bytesrdd)
