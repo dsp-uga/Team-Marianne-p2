20e5735d59ec54eb5dbade67d8f291660bf7c998
Vinay Kumar Bingi <vinay.bingi@gmail.com>
2018-02-09 21:28:07 -0500

    adds a driver file that calls all the utility and classifier functions

---
+++ src/__main__.py
@@ -0,0 +1,18 @@
+
+from pyspark import SparkContext, SparkConf
+
+from src.preprocess.BytePreProcessing import ByteFeatures
+
+def main(args):
+	
+	# Refer https://spark.apache.org/docs/latest/configuration.html for additional changes to config 
+	config = SparkConf().setAppName("team-marianne-p2")\
+				.set("spark.hadoop.validateOutputSpecs", "false")
+
+	sc = SparkContext.getOrCreate(config)
+
+	data, labels = ByteFeatures(sc).load_data(args.dataset, args.labels)	# converts byte data and labels to RDDs
+	# updates data and labels rdds. labels will be updated to (hash, label) and data will be updated to (hash, bigram_dict)
+	data, labels = ByteFeatures(sc).transform_data(data, args.bytestrain, labels)
+	# write data rdd to file -> makes it easier to retrieve processed data
+	ByteFeatures(sc).write_to_file(data, args.bytesrdd)
