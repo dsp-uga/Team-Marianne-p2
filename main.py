7db91d43eb001b99bb255c6812e048ade82fc2c5
Vinay Kumar <vinay.bingi@gmail.com>
2018-02-15 02:45:25 -0500

    makes gs: paths as default

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ import src.__main__
 parser = argparse.ArgumentParser(description='Team Marianne solution for Malware Classification')
 
 # All args are optional. Default values are set for each argument
-parser.add_argument ("-d", "--dataset", default="data/sample/X_vs_train.txt",
+parser.add_argument ("-d", "--dataset", default="gs://uga-dsp/project2/files/X_small_train.txt",
     help = "Path to text file containing hash of documents in training set")
 
-parser.add_argument ("-l", "--labels", default="data/sample/y_vs_train.txt",
+parser.add_argument ("-l", "--labels", default="gs://uga-dsp/project2/files/y_small_train.txt",
     help = "Path to text file containing labels of documents in training set")
 
-parser.add_argument ("-t", "--testset", default="data/sample/X_vs_test.txt",
+parser.add_argument ("-t", "--testset", default="gs://uga-dsp/project2/files/X_small_test.txt",
     help = "Path to text file containing hash of documents in testing set")
 
 parser.add_argument ("-e", "--evaluate", action="store_true",
     help = "Set this to evaluate accuracy on the test set")
 
-parser.add_argument ("-m", "--testlabels", default="data/sample/y_vs_test.txt",
+parser.add_argument ("-m", "--testlabels", default="gs://uga-dsp/project2/files/y_small_test.txt",
     help = "Path to text file containing labels of documents in testing set."
             "If evaluate is set true, this file is compared with classifier output")
 
-parser.add_argument ("-a", "--asmtrain", default="data/sample/TrainAsm/",
+parser.add_argument ("-a", "--asmtrain", default="gs://uga-dsp/project2/data/asm/",
     help = "Path to directory that contains asm documemts of training set")
 
-parser.add_argument ("-at", "--asmtest", default="data/sample/TestAsm/",
+parser.add_argument ("-at", "--asmtest", default="gs://uga-dsp/project2/data/asm/",
     help = "Path to directory that contains asm documemts of testing set")
 
-parser.add_argument ("-b", "--bytestrain", default="data/sample/TrainBytes/",
+parser.add_argument ("-b", "--bytestrain", default="gs://uga-dsp/project2/data/bytes/",
     help = "Path to directory that contains bytes documemts of training set")
 
-parser.add_argument ("-bt", "--bytestest", default="data/sample/TestBytes/",
+parser.add_argument ("-bt", "--bytestest", default="gs://uga-dsp/project2/data/bytes/",
     help = "Path to directory that contains bytes documemts of testing set")
 
 parser.add_argument ("-A", "--asmrdd", default="data/sample/asm_rdd.txt",
