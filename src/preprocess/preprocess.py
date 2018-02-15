7a8a417421df14ee4236bf2c59efb5c0474800d2
Vinay Kumar <vinay.bingi@gmail.com>
2018-02-15 02:49:33 -0500

    include preprocessing functions for asm data

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -13,7 +13,10 @@ def process_byte_data(byte_data):
     this function excludes all the irrelevant information from a byte file
     returns only the hexadecimals seperated with a whitespace
     """
+
+    # substitues all strings with length >= 3 to ''. basically, eliminating pointers in point files
     single_string = re.sub(r'\b\w{3,}\b', '', byte_data)
+    # substitutes '\n' with space
     single_string = re.sub('\n', ' ', single_string)
     return single_string
 
@@ -30,4 +33,29 @@ def byte_tokenize(hexstring):
 	retlist = []
 	for i in grams:
 		retlist +=  list(ngrams(hexlist, i))
+	return retlist
+
+def asm_opcodes(asm_data):
+	"""
+	this function takes a asm file data as string, extracts the opcodes
+	returns: list of opcodes in that order
+	"""
+
+	# gives all opcodes. assuming opcodes are followed by seven spaces in asm files
+	opcodes = re.findall('\s{7}(\w+)', asm_data)
+	opcodes = [re.sub('_.*', '', i) for i in opcodes]
+	return opcodes
+
+def asm_tokenize(opcodes):
+	"""
+	this function takes a opcodes list and returns a list of ngram tuples
+	ngram parameter is a "magic number" here
+	can be changed and checked for accuracy
+	return list contains tuples of unigram, bigram, trigram, ... n-gram
+	"""
+	n = 2	# may need to change depending on accuracy
+	grams = [i+1 for i in range(n)]
+	retlist = []
+	for i in grams:
+		retlist +=  list(ngrams(opcodes, i))
 	return retlist
